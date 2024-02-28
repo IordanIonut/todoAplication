19af15ae4dda7714e48e54ff61662762e03bbd42
Iordan Ionut <mariusionut879@yahoo.com>
2024-02-28 13:37:59 +0200

    init

---
+++ todo Backend/src/main/java/com/example/todo/todo/Model/Todos.java
@@ -0,0 +1,4 @@
+package com.example.todo.todo.Model;
+
+public class Todos {
+}
---
+++ todo Backend/src/main/java/com/example/todo/todo/Model/Users.java
@@ -0,0 +1,19 @@
+package com.example.todo.todo.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+}
